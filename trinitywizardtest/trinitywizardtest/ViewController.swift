2f07f148f080db0787e7ca1b1dc12032eb8d8aa2
Zul Zain <zulhilmi.zain@thestar.com.my>
2024-04-21 09:56:11 +0800

    Initial Commit

---
+++ trinitywizardtest/trinitywizardtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trinitywizardtest
+//
+//  Created by Mohd Zulhilmi Bin Mohd Zain on 21/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
